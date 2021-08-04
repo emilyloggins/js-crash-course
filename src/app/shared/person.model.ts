d2f5bd6a9d8c880b36660f89683a572a72745d8b
Emily Loggins <em.loggins7@gmail.com>
2021-08-03 20:34:28 -0400

    map and filter

---
+++ src/app/shared/person.model.ts
@@ -0,0 +1,8 @@
+export class Person {
+    id: number = 0;
+    firstName: string = '';
+    lastName: string = '';
+    email: string = '';
+    gender: string = '';
+    ipAddress: string = '';
+}
